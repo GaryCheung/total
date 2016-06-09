5d48ec5f12d6d6f0f074e538c2e522b863784d1d
Gary_Cheung <sirius.ziham@gmail.com>
2016-06-09 21:37:42 +0800

    add lianjia rent data

--- toal_PHPsamedb.py
+++ toal_PHPsamedb.py
@@ -918,8 +918,104 @@ def mysql_insert(source,price):
     finally:
         connection.close()
 
-print('Execute on---------',present_time)
+print('Gold Execute on---------',present_time)
 source = ['hexun']
 price = get_gold_price(url[0])
 #print(price)
 mysql_insert(source[0],price)
+
+config = {
+    'host':'127.0.0.1',
+    'port':8889,
+    'user':'root',
+    'password':'root',
+    'db':'house_rent',
+    'charset':'utf8',
+    'unix_socket':'/Applications/MAMP/tmp/mysql/mysql.sock'
+}
+
+present_date = datetime.now().date()
+
+def delete_today_data(config):
+    connection = pymysql.connect(**config)
+    try:
+        with connection.cursor() as cursor:
+            # 执行sql语句，插入记录
+            sql = "DELETE FROM house_rent WHERE date = '%s' and source = 'lianjia'" %(present_date)
+            cursor.execute(sql)
+            # 没有设置默认自动提交，需要主动提交，以保存所执行的语句
+        connection.commit()
+    finally:
+        connection.close()
+    print('-----------------------delete success!----------------','\n')
+
+def get_lianjia_rent_url(url_number,housename):
+    print('--------------------------',url_number,housename)
+    urls = []
+    url_begin = 'http://sh.lianjia.com/zufang/rs'
+    for i in range(1,url_number+1):
+        urls.append(i)
+        url_middle = quote(housename[i-1])
+        #print(url_middle)
+        urls[i-1] = url_begin + url_middle
+    return urls
+
+def get_lianjia_house(urls,source):
+    for url in urls:
+        print('current url:---------',url)
+        web_data = requests.get(url)
+        soup = BeautifulSoup(web_data.text,'lxml')
+        house_page = soup.select('body > div.wrapper > div.page-box.house-lst-page-box > a')
+        #print('house_page------------',house_page)
+        for page in house_page:
+            if page.get_text().isdigit():
+                pages = page.get_text()
+            else:
+                break
+        if house_page==[]:
+            pages=1
+        url_base = 'http://sh.lianjia.com/zufang/'
+        for page in range(1,int(pages)+1):
+            more_page = 'd'+str(page)
+            urls = re.split(url_base,url)
+            url = url_base + more_page + urls[1]
+            #print('real url is ----------------',url)
+            web_data = requests.get(url)
+            soup = BeautifulSoup(web_data.text,'lxml')
+            house_name = soup.select('#house-lst > li > div.info-panel > div.col-1 > div.where > a > span')
+            house_price = soup.select('#house-lst > li > div.info-panel > div.col-3 > div.price > span')
+            house_area = soup.select('#house-lst > li > div.info-panel > div.col-1 > div.where > span:nth-of-type(2)')
+            house_layout = soup.select('#house-lst > li > div.info-panel > div.col-1 > div.where > span:nth-of-type(1)')
+            #print(house_name,house_price,house_area,house_layout)
+            for name,price,area,layout in zip (house_name,house_price,house_area,house_layout):
+                print(name,price,area,layout)
+                connection = pymysql.connect(**config)
+                name = name.get_text().strip()
+                name = name.encode('UTF-8', 'ignore')
+                #price = price.get_text()
+                price = re.findall(r'(\w*[0-9]+\.*[0-9]+)\w*',price.get_text())
+                area = re.findall(r'(\w*[0-9]+\.*[0-9]+)\w*',area.get_text())
+                layout = layout.get_text()
+                print(price,'-----',area,'--------',layout,'-----------\n')
+                try:
+                     with connection.cursor() as cursor:
+                     # 执行sql语句，插入记录
+                         sql = 'INSERT INTO house_rent (date, name, price, area, source, layout) VALUES (%s, %s, %s, %s, %s, %s)'
+                         cursor.execute(sql, (present_date, name, price, area, source, layout))
+                     # 没有设置默认自动提交，需要主动提交，以保存所执行的语句
+                     connection.commit()
+                finally:
+                     connection.close()
+        time.sleep(1)
+
+#delete_today_data(config)
+url_number = len(house_name)
+source =['fangdd','lianjia','iwjw']
+print('execute time:-------------------',present_date)
+
+lianjia_url = get_lianjia_rent_url(url_number,house_name)
+#print('url is --------------',lianjia_url)
+get_lianjia_house(lianjia_url,source[1])
+
+
+print("All Done!!")
